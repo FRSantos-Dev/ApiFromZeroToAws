99b38e784fdba9073809237c07fea891c795883d
Felipe Rocha <feliperocha.s@outlook.com>
2023-11-06 15:42:22 -0300

    PersonMapper received methods convertEntityToVO & ConvertToEntity

--- restaapitoaws/src/main/java/br/com/rocha/restaapitoaws/mapper/custom/PersonMapper.java
+++ restaapitoaws/src/main/java/br/com/rocha/restaapitoaws/mapper/custom/PersonMapper.java
@@ -1,4 +1,31 @@
 package br.com.rocha.restaapitoaws.mapper.custom;
 
+import br.com.rocha.restaapitoaws.data.vo.v2.PersonVOV2;
+import br.com.rocha.restaapitoaws.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
 public class PersonMapper {
+
+    public PersonVOV2 convertEntityToVo(Person person){//Recebe uma Entidade e devolve VO
+        PersonVOV2 vo = new PersonVOV2();
+        vo.setId(person.getId());
+        vo.setAddress(person.getAddress());
+        vo.setBirthDay(new Date());
+        vo.setLastName(person.getLastName());
+        vo.setGender(person.getGender());
+        return vo;
+    }
+
+    public Person convertVoToEntity(PersonVOV2 person){//Converter VO para Entidades
+        Person entity = new Person();
+        entity.setId(person.getId());
+        entity.setAddress(person.getAddress());
+        ///vo.setBirthDay(new Date());
+        entity.setLastName(person.getLastName());
+        entity.setGender(person.getGender());
+        return entity;
+    }
 }
